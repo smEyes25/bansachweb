56a1b378eeb484d307fecc1910282c175aef3164
Long Nguyen <longnguyendev.225@gmail.com>
2023-03-04 09:01:47 +0700

    cors3

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'https://bookstore-ten-gamma.vercel.app',
     methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization',
+    allowedHeaders: 'X-Requested-With,content-type, Authorization',
   });
   await app.listen(3000);
 }
