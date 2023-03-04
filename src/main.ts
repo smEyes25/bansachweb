a714d8934243c54b092c6c8a16159ff4ef0980ba
Long Nguyen <longnguyendev.225@gmail.com>
2023-03-04 08:41:14 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app: any = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://bookstore-ten-gamma.vercel.app',
+    credentials: false,
+  });
   await app.listen(3000);
 }
 bootstrap();
