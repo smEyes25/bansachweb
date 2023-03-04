c97bf3d91490bdddbde185c60f7679c202af97ae
Long Nguyen <longnguyendev.225@gmail.com>
2023-03-04 09:36:10 +0700

    cors765486

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://bansachweb.vercel.app/v1/api/profile',
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'UPDATE'],
-    credentials: true,
-    allowedHeaders: 'Access-Control-Allow-Origin',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
