e9e3bca6b948619288655df21b2ed55d858db08c
Long Nguyen <longnguyendev.225@gmail.com>
2023-03-04 10:22:31 +0700

    cors323242

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { Response } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  app.use((req: any, res: any) => {
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+  });
 
   await app.listen(3000);
 }
