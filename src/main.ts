5d3d3966dc2246a5035bdcd60da21560fda25f06
Long Nguyen <longnguyendev.225@gmail.com>
2023-03-07 13:53:58 +0700

    abc

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: false,
-    },
-  });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
 
   await app.listen(3000);
